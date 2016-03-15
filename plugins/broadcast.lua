2c5aacb09697433ef4d2790d452025c11ade127a
ramin <r.harandi.rh.v@gmail.com>
2016-03-15 14:51:57 +0330

    Update broadcast.lua

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -1,10 +1,12 @@
 local function run(msg, matches)
-	if matches[1] == 'bc' and is_admin(msg) then
+	if matches[1] == 'send' and is_owner(msg) then
 		local response = matches[3]
 		send_large_msg("chat#id"..matches[2], response)
+		send_large_msg("user#id"..matches[2], response)
+		send_large_msg(get_receiver(msg), "sent!")
 	end
 	if matches[1] == 'broadcast' then
-		if is_sudo(msg) then -- Only sudo !
+		if is_sudo(msg) then 
 			local data = load_data(_config.moderation.data)
 			local groups = 'groups'
 			local response = matches[2]
@@ -18,8 +20,8 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/](broadcast) +(.+)$",
-    "^[!/](bc) (%d+) (.*)$"
+    "^[!/#](broadcast) +(.+)$",
+    "^[!/#](send) (%d+) (.*)$"
   },
   run = run
 }
