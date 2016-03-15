f8f44f921ae331b13445f7ac67328f10c80c4086
ramin <r.harandi.rh.v@gmail.com>
2016-03-15 14:47:32 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -75,8 +75,8 @@ return {
     "^[!/](import) (.*)$",
     "^[!/](unblock) (%d+)$",
     "^[!/](block) (%d+)$",
-	"^[!/](markread) (on)$",
-	"^[!/](markread) (off)$",
+    "^[!/](markread) (on)$",
+    "^[!/](markread) (off)$",
     "^[!/](setbotphoto)$",
 	"%[(photo)%]"
   },
