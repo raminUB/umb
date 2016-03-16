0e5ed3ec8d81f6881524646393dcbbb9cdf9ebde
ramin <r.harandi.rh.v@gmail.com>
2016-03-17 01:03:18 +0330

    Update lock_chat.lua

--- plugins/lock_chat.lua
+++ plugins/lock_chat.lua
@@ -1,31 +1,19 @@
 
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
-kicktable = {}
-
-
-local function run(msg, matches)
-    if is_momod(msg) then
-        return msg
-    end
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_chat'] then
-                lock_chat = data[tostring(msg.to.id)]['settings']['lock_chat']
-            end
-        end
-    end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_chat == "yes" then
-        send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " chat is not allowed here!")
-        chat_del_user(chat, user, ok_cb, true)
-    end
+local function run(msg)   
+    local data = load_data(_config.moderation.data)   
+     if data[tostring(msg.to.id)]['settings']['lock_chat'] == 'yes' then
+     if not is_momod(msg) then
+    send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " chat is not allowed here")
+    chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msgads = 'ForbiddenAdText'
+    local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+      end
+   end
 end
- 
 return {
-  patterns = {
-  "(.+)"
- },
-  run = run
+patterns = {
+ "(.*)"
+},
+run = run
 }
