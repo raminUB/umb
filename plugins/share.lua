339b306f9e2d058fb8cb1767f2ec4bd74117e4ce
ramin <r.harandi.rh.v@gmail.com>
2016-03-17 12:52:40 +0330

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+989117553119", "UB", " anti spam", ok_cb, false)
+send_contact(get_receiver(msg), "+989390933763", "UB", " Helper 1", ok_cb, false)
+end
+
+return {
+patterns = {
+"^!ub$"
+
+},
+run = run
+}
+
+end
