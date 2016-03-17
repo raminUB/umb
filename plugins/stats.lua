4ed7ca9f65e8af7384926a58e0a558a105de815a
raminub <r.harandi.rh.v@gmail.com>
2016-03-18 02:53:24 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -90,10 +90,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'ub' then -- Put everything you like :)
+  if matches[1]:lower() == 'unlimited' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /ub ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /unlimited ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -119,7 +119,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "ub" then -- Put everything you like :)
+    if matches[2] == "unlimited" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -140,8 +140,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (ub)",-- Put everything you like :)
-		"^[!/]([Uu]b)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (Unlimited)",-- Put everything you like :)
+		"^[!/]([Uu]nlimited)"-- Put everything you like :)
     }, 
   run = run
 }
