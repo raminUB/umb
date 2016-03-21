a5ad70d5c4f4287b151ae7af11721e9ecd45b017
raminub <r.harandi.rh.v@gmail.com>
2016-03-21 19:39:51 +0430

    Update lock_photo.lua

--- plugins/lock_photo.lua
+++ plugins/lock_photo.lua
@@ -1,11 +1,8 @@
-
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
 kicktable = {}
-
 
 local function run(msg, matches)
     if is_momod(msg) then
-        return msg
+        return
     end
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
@@ -18,14 +15,14 @@ local function run(msg, matches)
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
     if lock_ax == "yes" then
-        send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " images share is not allowed here!")
+        send_large_msg(chat, 'share image is not allowed here!!')
         chat_del_user(chat, user, ok_cb, true)
     end
 end
  
 return {
   patterns = {
-  "%[(photo)%]"
- },
+    "%[(photo)%]"
+},
   run = run
 }
