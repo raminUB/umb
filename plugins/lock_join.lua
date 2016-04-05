deab50cb679453a367cc839ba79a8a73c324eea2
ramtininfo2 <r.harandi.rh.v2@gmail.com>
2016-04-05 16:37:56 +0430

    fix bugs

--- plugins/lock_join.lua
+++ plugins/lock_join.lua
@@ -1,29 +1,23 @@
-kicktable = {}
-
-
-local function run(msg, matches)
+local function run (msg, matches)
     if is_momod(msg) then
-        return msg
+        return nil
     end
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
+local data = load_data(_config.moderation.data)
+   if matches[1] == 'chat_add_user_link' then
+        local user_id = msg.from.id
+        if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_join'] then
-                lock_member = data[tostring(msg.to.id)]['settings']['lock_join']
-            end
+        if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
+      kick_user(user_id, msg.to.id)
+      send_large_msg(get_receiver(msg), "Member add is lock")
+                    end
+                end
+            end   
         end
     end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_join == "yes" then
-      send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " member add is lock!")
-      chat_del_user(chat, user, ok_cb, true)
-    end
-end
- 
 return {
   patterns = {
-  "^!!tgservice (chat_add_user_link)$"
- },
+    "^!!tgservice (chat_add_user_link)$"
+  },
   run = run
 }
